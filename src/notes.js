2daf304cad728239e04d58a731e3f5059a3fb9e2
Shuva <34592080+Shuvax@users.noreply.github.com>
2023-10-05 13:37:07 -0400

    deploy website

---
+++ src/notes.js
@@ -0,0 +1,33 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content: "Q. how many programmers does it to change a lightbulb?",
+  },
+
+  {
+    key: 2,
+    title: "Delegation",
+    content: "Q. how many programmers does it to change a lightbulb?",
+  },
+
+  {
+    key: 3,
+    title: "Delegation",
+    content: "Q. how many programmers does it to change a lightbulb?",
+  },
+
+  {
+    key: 4,
+    title: "Delegation",
+    content: "Q. how many programmers does it to change a lightbulb?",
+  },
+
+  {
+    key: 5,
+    title: "Delegation",
+    content: "Q. how many programmers does it to change a lightbulb?",
+  },
+];
+
+export default notes;
